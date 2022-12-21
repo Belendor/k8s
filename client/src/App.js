4fa90d9d9848da33d1ca7be43c5f940b7418f1bb
Belendor <dwarf11@gmail.com>
2022-12-21 09:07:26 +0200

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            OMG it Works? Updated
+            OMG it Works? Updated2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>

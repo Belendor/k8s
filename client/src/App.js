155af80ff5290caa352f4c0a0161911a7cdbcaba
Belendor <dwarf11@gmail.com>
2022-12-27 10:35:32 +0200

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            OMG it Works? Updated3
+            OMG it Works? Updated4
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
